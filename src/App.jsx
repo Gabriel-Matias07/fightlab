36f0a45e78bf52b28c41f0a6309763bb47f91307
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2025-05-16 01:52:38 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,32 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>FightLab</h1>
+      <h2>Estrutura Básica do Projeto 'FightLab' Sendo Desenvolvida</h2>
+
+      <div className="contributors">
+        <table>
+          <tr> 
+            <td align="center"> 
+              <img src="https://avatars.githubusercontent.com/u/124216130?v=4" width="100px;" alt="Gabriel"/><br /> 
+              <strong><a href="https://github.com/Gabriel-Matias07">Gabriel</a></strong> 
+            </td> 
+            <td align="center"> 
+              <img src="https://avatars.githubusercontent.com/u/173968372?v=4" width="100px;" alt="Rayana Lima"/><br /> 
+              <strong><a href="https://github.com/Rayanagmss">Rayana Lima</a></strong> 
+            </td> 
+            <td align="center"> 
+              <img src="https://avatars.githubusercontent.com/u/99146426?v=4" width="100px;" alt="Moisés Iatagan"/><br /> 
+              <strong><a href="https://github.com/miyatakuun">Moisés Iatagan</a></strong> 
+            </td> 
+            <td align="center"> 
+              <img src="https://avatars.githubusercontent.com/u/124687497?v=4" width="100px;" alt="Victor Anderson"/><br /> 
+              <strong><a href="https://github.com/VictorNicolau-coder">Victor Anderson</a></strong> 
+            </td> 
+          </tr> 
+        </table>
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
