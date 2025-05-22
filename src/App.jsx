664165d7c8583b8a6d8b1946e3796ad24abc3e8d
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2025-05-22 00:40:11 -0300

    feat/adiciona estrutura da home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Home from './components/Home';
+import Home from './components/home/Home';
 
 export default function App(){
   return(
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,10 +3,20 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
-      <nav className='nav'>
+      <span className='span'>
       <h1 className='upperCase'>Encontre Grandes Nomes do Box</h1>
       <h2 className='upperCase'>Histórico, estatísticas e curiosidades dos <br /> maiores boxeadores do mundo</h2>
-      </nav>
+      </span>
+      <button className="btn">Pesquisar Atletas</button>
+      <button className="btn">Sobre Nós</button>
+    </div>
+
+  );
+}
+
+function Button() {
+  return (
+    <div>
       <button className="btn">Pesquisar Atletas</button>
       <button className="btn">Sobre Nós</button>
     </div>
