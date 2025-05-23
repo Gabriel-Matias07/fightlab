2d25e40610291769967cd1494aeb244ff7959ec2
Rayana <rayanalimag@gmail.com>
2025-05-22 22:26:09 -0300

    feat:adiciona resultado busca

---
+++ src/components/resultadoBusca/ResultadoBusca.jsx
@@ -0,0 +1,11 @@
+import './ResultadoBusca.css';
+
+function ResultadoBusca(){
+    return(
+        <div>
+            <h1 className="h1">Teste</h1>
+        </div>
+    );
+};
+
+export default ResultadoBusca;
