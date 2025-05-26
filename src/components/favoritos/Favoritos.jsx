39891c9ace53375be1cb452cd3b8ba07bb9977bd
Moisés Iatagan <freakyatagan@gmail.com>
2025-05-26 11:51:26 -0300

    Favoritos funcionando e botão de voltar para home adicionado

--- src/components/favoritos/Favoritos.jsx
+++ src/components/favoritos/Favoritos.jsx
@@ -1,36 +1,37 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./Favoritos.css";
+import { Link } from 'react-router-dom';
 
 const Favoritos = () => {
-  const [favoritos, setFavoritos] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const favoritosSalvos = JSON.parse(localStorage.getItem('favoritos')) || [];
-    setFavoritos(favoritosSalvos);
-  }, []);
+  const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
 
   return (
     <div className="container">
+      <Link to="/" className="voltar-home">
+        <span className="seta">&#8592;</span> Voltar para Home
+      </Link>
+
       <h1 className="titulo">ATLETAS FAVORITOS</h1>
       <div className="linha-vermelha"></div>
       <div className="cards">
         {favoritos.length > 0 ? (
           favoritos.map((atleta) => (
             <div key={atleta.id} className="card-atleta">
-              <img src={atleta.photo || 'https://jacksonwink.com/wp-content/uploads/2023/09/642f3a306a28fe28935d27cb_jackson-wink-mma-fighter-profile-image-placeholder-male-01-p-20001-scaled-scaled.webp'} alt={atleta.name} className="imagem-atleta" />
+              <img src={atleta.photo || 'https://jacksonwink.com/wp-content/uploads/2023/09/642f3a306a28fe28935d27cb_jackson-wink-mma-fighter-profile-image-placeholder-male-01-p-20001-scaled-scaled.webp'} 
+                   alt={atleta.name} 
+                   className="imagem-atleta" />
               <div className="info-atleta">
                 <div className="nome-atleta">{atleta.name}</div>
-                <p>{atleta.country?.name || 'País desconhecido'}</p>
+                <p>{atleta.country || 'País desconhecido'}</p>
                 <p>{atleta.category || 'Categoria desconhecida'}</p>
-                <p>{`${atleta.wins?.total || 0}V - ${atleta.losses?.total || 0}D - ${atleta.draws || 0}E`}</p>
-                <button onClick={() => navigate(`/atleta/${atleta.id}`)}>Ver perfil</button>
+                <p>{`${atleta.wins || 0}V - ${atleta.losses || 0}D - ${atleta.draws || 0}E`}</p>
+                <Link to={`/atleta/${atleta.id}`}>
+                  <button>Ver perfil</button>
+                </Link>
               </div>
             </div>
           ))
         ) : (
-          <p>Nenhum atleta favorito encontrado.</p>
+          <p>Você ainda não tem favoritos.</p>
         )}
       </div>
     </div>
